4bbf8880796c555253a7f15e3f6ce9c3f637231c
Emerson <79228789+Emersh0w@users.noreply.github.com>
2025-11-11 21:36:28 -0300

    Added Rustlings

---
+++ rust/rustlings/02_functions/functions5.rs
@@ -0,0 +1,10 @@
+// TODO: Fix the function body without changing the signature.
+fn square(num: i32) -> i32 {
+    let num = num * num;
+    return num;
+}
+
+fn main() {
+    let answer = square(3);
+    println!("The square of 3 is {answer}");
+}
